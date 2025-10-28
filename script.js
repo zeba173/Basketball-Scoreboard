180d07432d3cd177d5f3551c726574f70af3e990
Zeba <shaikhzeba423@gmail.com>
2025-10-28 17:13:11 +0530

    Added the timer feature to Basketball Scoreboard

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let scoreA = 0
 let scoreB = 0
+let totalSeconds = 12 * 60;
+let timer;
+let isRunning = false;
 
 function addScore(team, points)
 {
@@ -18,4 +21,45 @@ function resetGame()
     scoreB = 0
     document.getElementById('scoreA').textContent = '00';
     document.getElementById('scoreB').textContent = '00';
+}
+
+function updateDisplay()
+{
+    let minutes = Math.floor(totalSeconds / 60);
+    let seconds = totalSeconds % 60;
+
+    document.getElementById('timer').textContent = 
+    minutes.toString().padStart(2, '0') + ":" + seconds.toString().padStart(2, '0');
+}
+
+function startTimer()
+{
+    if(isRunning) return;
+    isRunning = true;
+
+    timer = setInterval(() => {
+        if(totalSeconds > 0)
+            {
+                totalSeconds--;
+                updateDisplay();
+            } else {
+                clearInterval(timer);
+                isRunning = false;
+                alert("Time's Up!");
+            }
+    }, 1000);
+}
+
+function stopTimer()
+{
+    clearInterval(timer);
+    isRunning = false;
+}
+
+function resetTimer()
+{
+    clearInterval(timer);
+    isRunning = false;
+    totalSeconds = 12 * 60;
+    updateDisplay();
 }
